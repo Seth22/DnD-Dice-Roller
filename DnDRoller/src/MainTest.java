6f956981db72afaa8ea720766ebe72dbd6009b76
Seth22 <41117493+Seth22@users.noreply.github.com>
2022-10-12 20:14:02 -0400

    Added new test case

--- DnDRoller/src/MainTest.java
+++ DnDRoller/src/MainTest.java
@@ -11,7 +11,6 @@ class MainTest {
     assertEquals(Main.getMax(),200);
     System.out.println("Test 1 passed! Set Max is working!");
     }
-
     @Test
     @DisplayName("Test 2: Set Random")
     void test2() {
@@ -21,12 +20,32 @@ class MainTest {
         assertEquals(Main.getRandom(),739);
         System.out.println("Test 2 passed! Set Random is working!");
     }
-
     @Test
     @DisplayName("Test 3: Min test")
     void test3() {
         assertEquals(Main.min,1);
         System.out.println("Test 3 passed! Min is 1");
     }
+    @Test
+    @DisplayName("Test 4: digit puller ")
+    void test4() {
+        assertEquals(4,49/10);
+        assertEquals(9,49%10);
 
+        assertEquals(9,99/10);
+        assertEquals(9,99%10);
+
+        assertEquals(9,98/10);
+        assertEquals(8,98%10);
+
+        assertEquals(9,978/100);
+        assertEquals(7,((978%100)/10));
+        assertEquals(8,978%10);
+
+        assertEquals(9,999/100);
+        assertEquals(9,((999%100)/10));
+        assertEquals(9,999%10);
+
+        System.out.println("Test 4 passed! Digit puller works!");
+    }
 }
